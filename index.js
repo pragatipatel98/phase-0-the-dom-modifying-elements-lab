10c4009085c4d8b686823d079cbdbf82b6757fad
Pragati Patel <pragatinpatel@gmail.com>
2023-03-12 09:35:26 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Pragati Patel is the champion";
